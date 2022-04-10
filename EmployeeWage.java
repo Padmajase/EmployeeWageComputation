55c5eaf8374435f1549fc20e0cd2d7380af4cff5
Padmajase <padmajapawar7@gmail.com>
2022-04-09 20:54:30 -0700

    Added : Employeewage UC1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,22 @@
+import java.util.Random;
 
-import java.util.*;
-
-class EmployeeWage {
+class UC1 {
 	public static void main(String args[]) {
 
-	System.out.println("Welcome !");	
+	System.out.println("Welcome !");
+
+	Random rand = new Random();
+	int isEmployee = rand.nextInt(2);	
+	System.out.println(isEmployee);
+
+	if(isEmployee == 1)
+	{
+		System.out.println("Employee is Present");
+	}
+	else
+	{
+		System.out.println("Employee is Absent");
+	}
 
 	}
 }
