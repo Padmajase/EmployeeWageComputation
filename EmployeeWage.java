2e8ab771646f570ee0629ffa70a0d6fc929c7ab0
Padmajase <padmajapawar7@gmail.com>
2022-04-09 23:15:46 -0700

    Added : Employeewage UC5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 
-class UC4 {
+class UC5 {
 
 	static int wagePerHr = 20;
 	static int fullTimeHr = 8;
 	static int partTimeHr = 8;
+	static int dayPerMonth = 20;
 
 	public static void main(String args[]) {
 
@@ -14,30 +15,30 @@ class UC4 {
 	int isEmployee = rand.nextInt(3);	
 	System.out.println(isEmployee);
 	
-		switch (isEmployee) {
-		case 1 :
-		{
-			System.out.println("Employee is full time Present");
-			System.out.println("Daily full time Employee Wage = " +(wagePerHr * fullTimeHr));
+	switch (isEmployee) {
+	case 1 :
+	{
+		System.out.println("Employee is full time Present");
+		System.out.println("Daily full time Employee Wage = " +(wagePerHr * fullTimeHr * dayPerMonth));
 		
-		}
-		break;
-		case 2 :
-		{
-			System.out.println("Employee is part time Present");
-			System.out.println("Daily part time Employee Wage = " +(wagePerHr * partTimeHr));
-		}	
-		break;
-		case 0 :
-		{
-			System.out.println("Employee is Absent");
-			System.out.println("Daily Employee Wage = 0 " );
-		}
-		break;
-		default :
-			System.out.println("Invalid");
-		break;
+	}
+	break;
+	case 2 :
+	{
+		System.out.println("Employee is part time Present");
+		System.out.println("Daily part time Employee Wage = " +(wagePerHr * partTimeHr * dayPerMonth));
+	}	
+	break;
+	case 0 :
+	{
+		System.out.println("Employee is Absent");
+		System.out.println("Daily Employee Wage = 0 " );
+	}
+	break;
+	default :
+		System.out.println("Invalid");
+	break;
 
-		}
+	}
 	}
 }
