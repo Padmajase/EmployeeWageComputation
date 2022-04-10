a620d3212bc8799085ab41bd82c6c1da82afe8fe
Padmajase <padmajapawar7@gmail.com>
2022-04-09 21:00:07 -0700

    Added : Employeewage UC3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,29 @@
 import java.util.Random;
 
-class UC2 {
+class UC3 {
 
 	static int wagePerHr = 20;
 	static int fullTimeHr = 8;
+	static int partTimeHr = 8;
+
 	public static void main(String args[]) {
 
 	System.out.println("Welcome !");
 
 	Random rand = new Random();
-	int isEmployee = rand.nextInt(2);	
-	System.out.println(isEmployee);
+	int isEmployee = rand.nextInt(3);	
+	//System.out.println(isEmployee);
 
 	if(isEmployee == 1)
 	{
-		System.out.println("Employee is Present");
-		System.out.println("Daily Employee Wage = " +(wagePerHr * fullTimeHr));
+		System.out.println("Employee is full time Present");
+		System.out.println("Daily full time Employee Wage = " +(wagePerHr * fullTimeHr));
+		
+	}
+	else if(isEmployee == 2)
+	{
+		System.out.println("Employee is part time Present");
+		System.out.println("Daily part time Employee Wage = " +(wagePerHr * partTimeHr));
 		
 	}
 	else
